bfdd8eab0fa78637e5a1b5229094e12c0efdf40c
Igor Ming de Mesquita <igor.ming@gmail.com>
2021-11-14 17:36:53 -0300

    configure apolli client and fetch data

---
+++ pages/types.ts
@@ -0,0 +1,34 @@
+interface Site {
+  site_name_long: string;
+}
+
+interface Links {
+  article_link: string;
+  video_link: string;
+  mission_patch: string;
+}
+
+interface Rocket {
+  rocket_name: string;
+  rocket_type: string;
+}
+
+interface Ship {
+  id: string;
+  name: string;
+  image: string;
+}
+
+interface LaunchProps {
+  id: string;
+  mission_name: string;
+  launch_date_local: Date;
+  launch_site: Site;
+  links: Links;
+  rocket: Rocket;
+  ships: Ship[];
+}
+
+export interface HomeProps {
+  launches: LaunchProps[];
+}
---
+++ queries.ts
@@ -0,0 +1,26 @@
+export const LAUNCHES_QUERY = `
+  query GetPastLaunches {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+      launch_date_local
+      launch_site {
+        site_name_long
+      }
+      links {
+        article_link
+        video_link
+        mission_patch
+      }
+      rocket {
+        rocket_name
+        rocket_type
+      }
+      ships {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
